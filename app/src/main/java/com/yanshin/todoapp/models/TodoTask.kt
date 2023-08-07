40a00d7bfe46b4e590ca07992361c744c00c637c
Yan Shin <irrfan15@gmail.com>
2023-08-07 18:06:48 +0630

    Add model class

---
+++ app/src/main/java/com/yanshin/todoapp/models/TodoTask.kt
@@ -0,0 +1,18 @@
+package com.yanshin.todoapp.models
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.yanshin.todoapp.utils.Constants.DATABASE_TABLE
+
+@Entity(tableName = DATABASE_TABLE)
+data class TodoTask(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val title: String,
+    val description: String,
+    val priority: Priority,
+
+
+) {
+
+}
